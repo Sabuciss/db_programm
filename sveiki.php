55e444484fbb584f26846ee647eb2a0130ae5179
Sabuciss <162728378+Sabuciss@users.noreply.github.com>
2025-01-27 11:17:46 +0200

    Update sveiki.php

--- sveiki.php
+++ sveiki.php
@@ -1,21 +1,24 @@
 <?php
-session_start();
+session_start(); // Start the session
 
-// Check if the user is logged in
+// Check if the user is logged in (session variable set)
 if (!isset($_SESSION['username'])) {
+    // If not logged in, redirect to index.php (registration page)
     header('Location: index.php');
-    exit();
+    exit(); // Make sure no further code is executed after the redirect
 }
 
+// Get the username from the session
 $username = $_SESSION['username'];
 ?>
 
 <!DOCTYPE html>
 <html lang="lv">
 <head>
-  <title>Welcome</title>
+    <link rel="stylesheet" href="style.css">
+    <title>Welcome</title>
 </head>
 <body>
-  <h1>Welcome, <?php echo htmlspecialchars($username); ?>!</h1>
+    <h1>Welcome, <?php echo $username; ?>!</h1>
 </body>
 </html>
